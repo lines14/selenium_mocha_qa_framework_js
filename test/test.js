ba2b4e800b413d7f7563485a9054a32656a0ac24
Anton Lipatov <tonybass1414@yahoo.com>
2023-01-27 13:53:36 +0600

    update test to work with multiple inbound data

--- test/test.js
+++ test/test.js
@@ -1,40 +1,45 @@
 // to run the test you may execute console command "npm test" if preinstalled "mocha", "chai" and "js-yaml" modules
 
-const X = 'admin_users'; // 'regular_users' 'admin_users'
+const listOfSets = ['regular_users','admin_users'];
 
-const chai = require('chai');
-const yaml = require('js-yaml');
-const fs = require('fs');
+for (i in listOfSets) {
+    const X = listOfSets[i];
 
-const jsonFile = require(`/home/lines14/projects/a.lipatov/test/test data/${X}.json`);
+    const chai = require('chai');
+    const yaml = require('js-yaml');
+    const fs = require('fs');
 
-const fileContents = fs.readFileSync(`/home/lines14/projects/a.lipatov/test/test data/${X}.yaml`, 'utf8');
-const yamlFile = yaml.load(fileContents);
+    const jsonFile = require(`/home/lines14/projects/a.lipatov/test/test data/${X}.json`);
 
-describe('Validate user files:', function () {
-    it('X.json contains array of users. User must have id, name and may have adress', function () {
-        chai.expect(jsonFile[0]).to.deep.include.all.keys(['id', 'name']);
-    });
-    it('X.yaml contains array of users. User must have id, name and may have adress', function () {
-        chai.expect(yamlFile[0]).to.deep.include.all.keys(['id', 'name']);
-    });
-    it('All users listed in X.json are included in X.yaml', function () {
-        let yamlFileIds = [];
-        let jsonFileIds = [];
-        for (i in yamlFile) {
-            for (j in yamlFile[i]) {
-                if (typeof yamlFile[i][j] == 'number') {
-                    yamlFileIds.push(yamlFile[i][j])
+    const fileContents = fs.readFileSync(`/home/lines14/projects/a.lipatov/test/test data/${X}.yaml`, 'utf8');
+    const yamlFile = yaml.load(fileContents);
+
+    describe('Validate user files:', function () {
+        it(`${X}.json contains array of users. User has id, name and may have adress`, function () {
+            chai.expect(jsonFile[0]).to.deep.include.all.keys(['id', 'name']);
+        });
+        it(`${X}.yaml contains array of users. User has id, name and may have adress`, function () {
+            chai.expect(yamlFile[0]).to.deep.include.all.keys(['id', 'name']);
+        });
+        it(`all users listed in ${X}.json are included in ${X}.yaml`, function () {
+            let yamlFileIds = [];
+            let jsonFileIds = [];
+            for (i in yamlFile) {
+                for (j in yamlFile[i]) {
+                    if (typeof yamlFile[i][j] == 'number') {
+                        yamlFileIds.push(yamlFile[i][j])
+                    };
                 };
             };
-        };
-        for (i in jsonFile) {
-            for (j in jsonFile[i]) {
-                if (typeof jsonFile[i][j] == 'number') {
-                    jsonFileIds.push(jsonFile[i][j])
+            for (i in jsonFile) {
+                for (j in jsonFile[i]) {
+                    if (typeof jsonFile[i][j] == 'number') {
+                        jsonFileIds.push(jsonFile[i][j])
+                    };
                 };
             };
-        };
-        chai.assert.includeDeepMembers(yamlFileIds, jsonFileIds);
+            chai.assert.includeDeepMembers(yamlFileIds, jsonFileIds);
+        });
     });
-});
+};
+
